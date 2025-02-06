76cc5aa16addf9f1fa2288f6367255b792f72bbf
danish0990a <124906173+danish0990a@users.noreply.github.com>
2025-02-06 16:29:07 +0400

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to iht Community"];
-const statusTypes = [ 'dnd'];
+const statusMessages = ["🎧 Watching iht Community"];
+const statusTypes = ['dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
